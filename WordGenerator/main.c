0f7484e91bde4349011d0356e805117d807cc66a
NinjaCheetah <58050615+NinjaCheetah@users.noreply.github.com>
2021-08-19 18:06:18 -0400

    WordGenerator: made it impossible to end up with 0 words

--- WordGenerator/main.c
+++ WordGenerator/main.c
@@ -1,4 +1,18 @@
-// Copyright (C) 2021 NinjaCheetah
+/*theVaultC/WordGenerator "main.c"
+Copyright (C) 2021 NinjaCheetah
+Copyright (C) 2021 NCX Programming
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -38,7 +52,7 @@ int main(void){
     }
     else nextCharBeginsWord=0;
   }
-  numPrintWords=rand()%6;
+  numPrintWords=1+rand()%(7-1);
   printf("Out of %d total words, your %d words are:\n",numWords,numPrintWords);
   for(ii=0;ii<numPrintWords;ii++){
     printf("%s ",wordArray[rand()%nCountedWords]);
