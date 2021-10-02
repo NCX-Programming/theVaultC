fdb162214140c642968b886cf96b6cca63ef72a4
NinjaCheetah <58050615+NinjaCheetah@users.noreply.github.com>
2021-10-01 22:12:35 -0400

    Added the ability to specify the number of words to generate in WordGenerator

--- WordGenerator/main.c
+++ WordGenerator/main.c
@@ -18,6 +18,7 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.*/
 #include <time.h>
 // Code start
 int countWords(char *buffer,int size){
+  // Count number of words in the file based on newlines
   int numWords=0,i;
   for(i=0;i<size;i++){
     char c=buffer[i];
@@ -25,10 +26,13 @@ int countWords(char *buffer,int size){
   }
   return numWords;
 }
-int main(void){
+int main(int argc,char *argv[]){
+  // Seed rand()
   srand(time(0));
   char *buffer;
   int size;
+  // Open word list file
+  // Read contents to buffer and size to size
   FILE *fp=fopen("words.txt","rb");
   fseek(fp,0,SEEK_END);
   size=ftell(fp);
@@ -36,6 +40,7 @@ int main(void){
   fseek(fp,0,SEEK_SET);
   fread(buffer,size,1,fp);
   fclose(fp);
+  // Initialize all variables
   int numWords=countWords(buffer,size);
   char **wordArray=malloc(numWords*sizeof(char *));
   int nextCharBeginsWord=1;
@@ -43,6 +48,9 @@ int main(void){
   int i;
   int ii;
   int numPrintWords;
+  // Loop through the entire buffer based on the number of words
+  // returned by countWords() and figure out the starting
+  // position of each word in the array
   for(i=0;i<size;i++){
     if(nextCharBeginsWord==1)wordArray[nCountedWords++]=buffer+i;
     char c=buffer[i];
@@ -52,10 +60,27 @@ int main(void){
     }
     else nextCharBeginsWord=0;
   }
-  numPrintWords=1+rand()%(7-1);
-  printf("Out of %d total words, your %d words are:\n",numWords,numPrintWords);
-  for(ii=0;ii<numPrintWords;ii++){
-    printf("%s ",wordArray[rand()%nCountedWords]);
+  if(argc==2){
+    // If an argument was supplied, use that as the number of words to generate
+    numPrintWords=atoi(argv[1]);
+    if(numPrintWords==0){
+      printf("You can only input an integer!\n");
+      return(1);
+    }
+    printf("Out of %d total words, your %d words are:\n",numWords,numPrintWords);
+    for(ii=0;ii<numPrintWords;ii++){
+      printf("%s ",wordArray[rand()%nCountedWords]);
+    }
+    printf("\n");
   }
-  printf("\n");
+  else {
+    // If no argument was supplied, generate a random number of words to generate
+    numPrintWords=1+rand()%(7-1);
+    printf("Out of %d total words, your %d words are:\n",numWords,numPrintWords);
+    for(ii=0;ii<numPrintWords;ii++){
+      printf("%s ",wordArray[rand()%nCountedWords]);
+    }
+    printf("\n");
+  }
+  return(0);
 }
