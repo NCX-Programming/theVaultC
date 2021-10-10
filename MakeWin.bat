d68aec883300fcca2f59a71f81f0be31e4b3a67a
NinjaCheetah <58050615+NinjaCheetah@users.noreply.github.com>
2021-10-09 21:47:31 -0400

    Reverted changes and went back to cross compiling because windows is dumb

--- MakeWin.bat
+++
@@ -1,7 +0,0 @@
-mkdir bin
-cl /Wall GraphicStuffC\main.c /link /O2 /out:bin\GraphicStuffC.exe
-cl /Wall GuessMyNumberC\main.c /link /O2 /out:bin\GuessMyNumberC.exe
-cl /Wall PrintUnixTime\main.c /link /O2 /out:bin\PrintUnixTime.exe
-cl /Wall RNGTool\main.c /link /O2 /out:bin\RNGTool.exe
-cl /Wall WordGenerator\main.c /link /O2 /out:bin\WordGenerator.exe
-cp WordGenerator/words.txt bin/words.txt
